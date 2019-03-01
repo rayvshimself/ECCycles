87b9c96c45dacdde8d501744ac287d87379645b1
Mathew <m.ray.varkki@gmail.com>
2019-03-01 19:49:16 +0100

    Now able to spawn more ghosts!

---
+++ Assets/aMain/Scripts/GhostBehaviour.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GhostBehaviour : MonoBehaviour
+{
+    public float lerpSpeed = 2;
+    public int frameUpdate = 20;
+
+    private int index = 1;
+    private bool noOverflow = true;
+
+    private Vector3 actualPos;
+    private Vector3 nextPos;
+    
+    private float startTime;
+    private float journeyLength;
+    private float distCovered;
+    private float fracJourney;
+
+    private void Start()
+    {
+        frameUpdate = GhostData.frameUpdate;
+    }
+
+    private void FixedUpdate()
+    {
+        if (FixedTime.fixedFrameCount % frameUpdate == 0 && noOverflow)
+        {
+            startTime = Time.time;
+
+            actualPos = transform.localPosition;
+            nextPos = GhostData.posTracker[index];
+            journeyLength = Vector3.Distance(actualPos, nextPos);
+
+            if (index >= GhostData.posTracker.Count - 1)
+            {
+                noOverflow = false;
+            }
+            index++;
+        }
+
+
+        if (noOverflow)
+        {
+            distCovered = (Time.time - startTime) * lerpSpeed;
+            fracJourney = distCovered / journeyLength;
+            transform.localPosition = Vector3.Lerp(actualPos, nextPos, fracJourney);
+
+        }
+
+        
+    }
+}
--- Assets/aMain/Scripts/GhostData.cs
+++ Assets/aMain/Scripts/GhostData.cs
@@ -9,103 +9,128 @@ using Vector3 = UnityEngine.Vector3;
 
 public class GhostData : MonoBehaviour
 {
-    List<Vector3> posTracker = new List<Vector3>();
-
+    public static List<Vector3> posTracker = new List<Vector3>();
+    //List<GameObject> ghostList = new List<GameObject>();
+    public static int frameUpdate = 20;
+    
     public GameObject trackingStart;
     public GameObject trackingEnd;
     public GameObject ghostPrefab;
-    public float lerpSpeed = 1;
-    public int frameUpdate = 15;
 
     private Collider _startCol;
     private Collider _endCol;
     private bool trackingBool = false;
+
     private bool replayBool = false;
-    private int index = 1;
-    private bool noOverflow = true;
-
-    private GameObject ghost;
-    private Vector3 actualPos;
-    private Vector3 nextPos;
-    private float startTime;
-    private float _timeStartetdLerping;
-    private float journeyLength;
-
+//    private int index;
+//    private bool noOverflow = true;
+//
+//    private GameObject ghost;
+//    private int ghostCount = 0;
+//    private Vector3 actualPos;
+//    private Vector3 nextPos;
+//    private float startTime;
+//    private float _timeStartetdLerping;
+//    private float journeyLength;
+//
+//    private float distCovered;
+//    private float fracJourney;
 
     private void Awake()
     {
-        _startCol = trackingStart.GetComponent<Collider>();
+        trackingBool = true;
+        //_startCol = trackingStart.GetComponent<Collider>();
         _endCol = trackingEnd.GetComponent<Collider>();
     }
 
 
     private void OnTriggerEnter(Collider other)
     {
-        
-        if (other == _startCol)
-        {
-            trackingBool = true;
-            //start writing to List
-        }
+
+//        if (other == _startCol)
+//        {
+//            trackingBool = true;
+//            //start writing to List
+//        }
 
         if (other == _endCol)
         {
-            ghost = Instantiate(ghostPrefab, posTracker[1], Quaternion.identity);
-            //trackingBool = false;
-            replayBool = true;
+            // ghostList.Add(Instantiate(ghostPrefab, posTracker[1], Quaternion.identity));
+            Instantiate(ghostPrefab, posTracker[1], Quaternion.identity);
+
+            //ghost = Instantiate(ghostPrefab, posTracker[1], Quaternion.identity);
+            //replayBool = true;
             //stop writing in List and playback
         }
     }
-    
-//    private void Update()
-//    {
-//        if (replayBool && noOverflow && ghost != null)
-//        {
-//            _timeStartetdLerping = Time.time;
-//        }
-//    }
 
     private void FixedUpdate()
     {
-        if ((posTracker.Count - 1) == 1000)
+        if ((posTracker.Count - 1) >= 1000)
         {
             trackingBool = false;
         }
-
+        
         if (FixedTime.fixedFrameCount % frameUpdate == 0 && trackingBool)
         {
             posTracker.Add(this.transform.localPosition);
-            Debug.Log("posTracker Count: " + posTracker.Count);
+            //Debug.Log("posTracker Count: " + posTracker.Count);
         }
-
-        if (FixedTime.fixedFrameCount % frameUpdate == 0 && replayBool && noOverflow)
-        {
-            startTime = Time.time;
-//          actualPos = posTracker[index];
-            actualPos = ghost.transform.localPosition;
-            nextPos = posTracker[index];
-            journeyLength = Vector3.Distance(actualPos, nextPos);
-
-
-            if (index >= posTracker.Count-1)
-            {
-                noOverflow = false;
-            }
-            index++;
-        }
-        
-        if (replayBool && noOverflow && ghost != null)
-        {
-//            float timeSinceStarted = Time.time - _timeStartetdLerping;
-//            float percentageComplete = timeSinceStarted / 1f;
-//            Debug.Log(percentageComplete);
-//            ghost.transform.localPosition = Vector3.Lerp(actualPos,nextPos, percentageComplete);
-            
-            float distCovered = (Time.time - startTime) * lerpSpeed;
-            float fracJourney = distCovered / journeyLength;
-            //Debug.Log(fracJourney);
-            ghost.transform.localPosition = Vector3.Lerp(actualPos,nextPos, fracJourney);
-        }
-        
     }
+
+//    private void FixedUpdate()
+//    {
+//        if ((posTracker.Count - 1) >= 1000)
+//        {
+//            trackingBool = false;
+//        }
+//
+//        if (FixedTime.fixedFrameCount % frameUpdate == 0 && trackingBool)
+//        {
+//            posTracker.Add(this.transform.localPosition);
+//            Debug.Log("posTracker Count: " + posTracker.Count);
+//        }
+//
+//        if (ghost != null)
+//        {
+//            if (FixedTime.fixedFrameCount % frameUpdate == 0 && replayBool && noOverflow)
+//            {
+//                startTime = Time.time;
+//                //actualPos = posTracker[index];
+//
+//                actualPos = ghost.transform.localPosition;
+//                nextPos = posTracker[index];
+//                journeyLength = Vector3.Distance(actualPos, nextPos);
+//                //index[i]++;
+//            
+//                if (index >= posTracker.Count-1)
+//                {
+//                    noOverflow = false;
+//                }
+//                index++;
+//            }
+//
+//        
+//            if (replayBool && noOverflow)
+//            {
+////            float timeSinceStarted = Time.time - _timeStartetdLerping;
+////            float percentageComplete = timeSinceStarted / 1f;
+////            Debug.Log(percentageComplete);
+////            ghost.transform.localPosition = Vector3.Lerp(actualPos,nextPos, percentageComplete);
+//            
+//
+//                //Debug.Log(fracJourney);
+//
+//                distCovered = (Time.time - startTime) * lerpSpeed;
+//                fracJourney = distCovered / journeyLength;
+//                ghost.transform.localPosition = Vector3.Lerp(actualPos,nextPos, fracJourney);
+//
+//
+//            }
+//            
+//        }
+//    }
+
+
+
 }
